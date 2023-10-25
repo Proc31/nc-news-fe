64478393d39a257d979a465cab9cf4afc4552f70
Matthew Proctor <matt@Matts-Mac.local>
2023-10-25 11:25:29 +0100

    title bar first pass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,133 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import { Avatar } from '@mui/material';
 
-export default function Navbar() {
+export default function Navbar({ currentUser }) {
 	return (
-		<div>
-			<nav>
-				<Link to="/articles">Articles </Link>
-				<Link to="/topics">Topics </Link>
-				<Link to="/profile">Profile </Link>
-				<Link to="/users">Users </Link>
-			</nav>
-		</div>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Box
+						sx={{
+							display: 'grid',
+							gridTemplateColumns: '0.3fr auto 1fr 1fr 1fr auto',
+							gridTemplateRows: '1fr',
+						}}
+					>
+						<Box
+							sx={{
+								gridColumn: '1',
+								width: 80,
+								height: 80,
+							}}
+						>
+							<img
+								style={{ height: '100%' }}
+								src="https://icons.iconarchive.com/icons/iconarchive/cute-animal/256/Cute-Cat-icon.png"
+								alt="cool cat news icon"
+							/>
+						</Box>
+						<Box
+							sx={{
+								display: 'flex',
+								alignItems: 'center',
+								gridColumn: '2',
+							}}
+						>
+							<Typography
+								sx={{
+									fontFamily: 'gill',
+									fontSize: 60,
+								}}
+							>
+								Cool Cat News
+							</Typography>
+						</Box>
+						<Box
+							sx={{
+								gridColumn: '3',
+								display: 'flex',
+								alignItems: 'end',
+								justifyContent: 'end',
+							}}
+						>
+							<Link
+								to="/articles"
+								style={{ textDecoration: 'none' }}
+							>
+								<Typography sx={{ color: 'white' }}>
+									Articles
+								</Typography>
+							</Link>
+						</Box>
+						<Box
+							sx={{
+								gridColumn: '4',
+								display: 'flex',
+								alignItems: 'end',
+								justifyContent: 'center',
+							}}
+						>
+							<Link
+								to="/topics"
+								style={{ textDecoration: 'none' }}
+							>
+								<Typography sx={{ color: 'white' }}>
+									Topics
+								</Typography>
+							</Link>
+						</Box>
+						<Box
+							sx={{
+								gridColumn: '5',
+								display: 'flex',
+								alignItems: 'end',
+								justifyContent: 'start',
+							}}
+						>
+							<Link
+								to="/users"
+								style={{ textDecoration: 'none' }}
+							>
+								<Typography sx={{ color: 'white' }}>
+									Users
+								</Typography>
+							</Link>
+						</Box>
+						<Box
+							sx={{
+								display: 'flex',
+								paddingLeft: 75,
+								width: 80,
+								height: 90,
+								gridColumn: '6',
+							}}
+						>
+							<IconButton
+								sx={{
+									display: 'flex',
+									width: 80,
+									height: 80,
+									alignItems: 'center',
+								}}
+							>
+								<Avatar>
+									<img
+										style={{ height: '100%' }}
+										alt={currentUser.username}
+										src={currentUser.avatar_url}
+									/>
+								</Avatar>
+							</IconButton>
+						</Box>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 }
