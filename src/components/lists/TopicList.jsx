62414581711dc33c1cc13eb66bc3ad53d51eb751
Matthew Proctor <matt@Matts-Mac.local>
2023-10-27 15:36:40 +0100

    implemented topic page

--- src/components/lists/TopicList.jsx
+++ src/components/lists/TopicList.jsx
@@ -1,3 +1,70 @@
+import { useState, useEffect } from 'react';
+import { getTopics } from '../../utils';
+import { Stack, Fab, Tooltip } from '@mui/material';
+import Loading from '../page-items/Loading';
+import Topic from './list-items/Topic';
+import TopicDialog from '../page-items/TopicDialog';
+import AddIcon from '@mui/icons-material/Add';
+
 export default function TopicList() {
-	return;
+	const [topicList, setTopicList] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [open, setOpen] = useState(false);
+
+	useEffect(() => {
+		getTopics().then((response) => {
+			setTopicList(response);
+			setLoading(false);
+		});
+	}, [open]);
+
+	function handleClickOpen() {
+		setOpen(true);
+	}
+
+	function handleClose() {
+		setOpen(false);
+	}
+
+	const topics = topicList.map((topic, index) => {
+		return (
+			<div key={topic.slug + index}>
+				<Topic topic={topic} />
+			</div>
+		);
+	});
+
+	if (loading) {
+		return <Loading />;
+	} else {
+		return (
+			<>
+				<Tooltip title="Add New Topic">
+					<Fab
+						color="primary"
+						aria-label="add new topic"
+						onClick={handleClickOpen}
+						sx={{
+							position: 'fixed',
+							bottom: '40px',
+							right: '40px',
+						}}
+					>
+						<AddIcon />
+					</Fab>
+				</Tooltip>
+				<TopicDialog handleClose={handleClose} open={open} />
+				<Stack
+					spacing={1}
+					direction="row"
+					mx={4}
+					my={1}
+					useFlexGap
+					flexWrap="wrap"
+				>
+					{topics}
+				</Stack>
+			</>
+		);
+	}
 }
--- src/components/lists/list-items/Topic.jsx
+++ src/components/lists/list-items/Topic.jsx
@@ -1,3 +1,26 @@
-export default function Topic() {
-	return;
+import { Typography, Box } from '@mui/material';
+
+export default function Topic({ topic }) {
+	return (
+		<Box //Base box
+			mt={2}
+			sx={{
+				display: 'grid',
+				border: 1,
+				boxShadow: 10,
+				padding: 1,
+				height: 150,
+				width: 150,
+			}}
+		>
+			<Box>
+				<Typography sx={{ textTransform: 'capitalize' }}>
+					{topic.slug}
+				</Typography>
+			</Box>
+			<Box>
+				<Typography>{topic.description}</Typography>
+			</Box>
+		</Box>
+	);
 }
