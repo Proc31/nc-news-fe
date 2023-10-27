73123780c0aaf674fbc7116881e78d41fc1171e8
Matthew Proctor <matt@Matts-Mac.local>
2023-10-27 10:38:21 +0100

    refactored nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,135 +1,169 @@
-import { Link } from 'react-router-dom';
 import * as React from 'react';
-import {
-	Avatar,
-	IconButton,
-	Typography,
-	Toolbar,
-	Box,
-	AppBar,
-} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-export default function Navbar({ currentUser, searchParams }) {
+const pages = ['Articles', 'Topics', 'Users'];
+const settings = ['Profile', 'Log Out'];
+
+export default function NavBar({ currentUser, searchParams }) {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const navigate = useNavigate();
+
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = (event) => {
+		const pathname = event.target.innerText.toLowerCase();
+		let search = '';
+		if (pathname === 'articles') {
+			search = `?${searchParams}`;
+		}
+		navigate({
+			pathname: pathname,
+			search: search,
+		});
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = (event) => {
+		const pathname = event.target.innerText.toLowerCase();
+		navigate({ pathname: pathname });
+		setAnchorElUser(null);
+	};
+
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Box
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<img
+						width={70}
+						src="https://icons.iconarchive.com/icons/iconarchive/cute-animal/256/Cute-Cat-icon.png"
+						alt="cool cat news icon"
+					/>
+					<Typography
+						variant="h4"
+						noWrap
+						component="a"
+						href="/"
 						sx={{
-							display: 'grid',
-							gridTemplateColumns: '0.3fr auto 1fr 1fr 1fr auto',
-							gridTemplateRows: '1fr',
+							mr: 2,
+							display: { xs: 'none', md: 'flex' },
+							fontFamily: 'monospace',
+							fontWeight: 700,
+							letterSpacing: '.3rem',
+							color: 'inherit',
+							textDecoration: 'none',
 						}}
 					>
-						<Box
+						CoolCatNews
+					</Typography>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: 'flex', md: 'none' },
+						}}
+					>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
 							sx={{
-								gridColumn: '1',
-								width: 80,
-								height: 80,
+								display: { xs: 'block', md: 'none' },
 							}}
 						>
-							<img
-								style={{ height: '100%' }}
-								src="https://icons.iconarchive.com/icons/iconarchive/cute-animal/256/Cute-Cat-icon.png"
-								alt="cool cat news icon"
-							/>
-						</Box>
-						<Box
-							sx={{
-								display: 'flex',
-								alignItems: 'center',
-								gridColumn: '2',
-							}}
-						>
-							<Typography
-								sx={{
-									fontFamily: 'gill',
-									fontSize: 60,
-								}}
+							{pages.map((page) => (
+								<MenuItem
+									key={page}
+									onClick={handleCloseNavMenu}
+								>
+									<Typography textAlign="center">
+										{page}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: 'flex',
+						}}
+					>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: 'white', display: 'block' }}
 							>
-								Cool Cat News
-							</Typography>
-						</Box>
-						<Box
-							sx={{
-								gridColumn: '3',
-								display: 'flex',
-								alignItems: 'end',
-								justifyContent: 'end',
-							}}
-						>
-							<Link
-								to="/articles"
-								style={{ textDecoration: 'none' }}
-							>
-								<Typography sx={{ color: 'white' }}>
-									Articles
-								</Typography>
-							</Link>
-						</Box>
-						<Box
-							sx={{
-								gridColumn: '4',
-								display: 'flex',
-								alignItems: 'end',
-								justifyContent: 'center',
-							}}
-						>
-							<Link
-								to="/topics"
-								style={{ textDecoration: 'none' }}
-							>
-								<Typography sx={{ color: 'white' }}>
-									Topics
-								</Typography>
-							</Link>
-						</Box>
-						<Box
-							sx={{
-								gridColumn: '5',
-								display: 'flex',
-								alignItems: 'end',
-								justifyContent: 'start',
-							}}
-						>
-							<Link
-								to="/users"
-								style={{ textDecoration: 'none' }}
-							>
-								<Typography sx={{ color: 'white' }}>
-									Users
-								</Typography>
-							</Link>
-						</Box>
-						<Box
-							sx={{
-								display: 'flex',
-								paddingLeft: 75,
-								width: 80,
-								height: 90,
-								gridColumn: '6',
-							}}
-						>
+								{page}
+							</Button>
+						))}
+					</Box>
+
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open User Settings">
 							<IconButton
-								sx={{
-									display: 'flex',
-									width: 80,
-									height: 80,
-									alignItems: 'center',
-								}}
+								onClick={handleOpenUserMenu}
+								sx={{ p: 0 }}
 							>
-								<Avatar>
-									<img
-										style={{ height: '100%' }}
-										alt={currentUser.username}
-										src={currentUser.avatar_url}
-									/>
-								</Avatar>
+								<Avatar
+									alt={currentUser.username}
+									src={currentUser.avatar_url}
+								/>
 							</IconButton>
-						</Box>
+						</Tooltip>
+						<Menu
+							sx={{ mt: '45px' }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							{settings.map((setting) => (
+								<MenuItem
+									key={setting}
+									onClick={handleCloseUserMenu}
+								>
+									<Typography textAlign="center">
+										{setting}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
 					</Box>
 				</Toolbar>
-			</AppBar>
-		</Box>
+			</Container>
+		</AppBar>
 	);
 }
