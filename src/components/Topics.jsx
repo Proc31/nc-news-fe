b22ada7f973370984221ca1ee90600767c8cb2d2
Matthew Proctor <matt@Matts-Mac.local>
2023-10-27 13:49:43 +0100

    start of topics

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getTopics } from '../utils';
+import TopicList from './lists/TopicList';
+import { Box, Typography } from '@mui/material';
+
 export default function Topics() {
-	return;
+	const [topics, setTopics] = useState([]);
+
+	useEffect(() => {
+		getTopics().then((response) => {
+			setTopics(response);
+		});
+	});
+
+	return (
+		<>
+			<Box sx={{ alignItems: 'center', flexGrow: 1 }}>
+				<Typography
+					variant="h3"
+					noWrap
+					component="a"
+					href="/topics"
+					sx={{
+						display: 'flex',
+						fontFamily: 'monospace',
+						fontWeight: 700,
+						letterSpacing: '.5rem',
+						color: 'inherit',
+						textDecoration: 'none',
+						textTransform: 'uppercase',
+					}}
+				>
+					Topics
+				</Typography>
+			</Box>
+			<TopicList />
+		</>
+	);
 }
